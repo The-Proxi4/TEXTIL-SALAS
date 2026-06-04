d472e62c9372e488b472b287312cac02144a6444
The-Proxi4 <huaman22hm@gmail.com>
2026-06-04 18:11:25 -0500

    Fix Document.Create usage to call Compose lambda

--- Controllers/ReportsController.cs
+++ Controllers/ReportsController.cs
@@ -80,7 +80,7 @@ public class ReportsController : Controller
         {
             var doc = new textil_salas.Documents.ReportsPdfDocument(vm);
             QuestPDF.Settings.License = QuestPDF.Infrastructure.LicenseType.Community;
-            var pdfBytes = QuestPDF.Fluent.Document.Create(doc).GeneratePdf();
+            var pdfBytes = QuestPDF.Fluent.Document.Create(container => doc.Compose(container)).GeneratePdf();
             var fileName = reportType + "_report_" + DateTime.UtcNow.ToString("yyyyMMddHHmmss") + ".pdf";
             return File(pdfBytes, "application/pdf", fileName);
         }
